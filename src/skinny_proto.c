22d69a745ad75356c3914f2742ba78881a9a4cca
Stas Kobzar <stas@modulis.ca>
2016-09-12 21:16:04 -0400

    License inserted.

--- src/skinny_proto.c
+++ src/skinny_proto.c
@@ -1,3 +1,23 @@
+/**
+ * skinnycat -- Skinny Cisco protocol command line utility
+ * Copyright (C) 2016, Stas Kobzar <stas@modulis.ca>
+ *
+ * This file is part of skinnycat.
+ *
+ * skinnycat is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * skinnycat is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with skinnycat.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "skinny_proto.h"
 #include <apr_strings.h>
 
